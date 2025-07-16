2416f4cbb887d734a1fcdd64e10e1e1657791cf2
Sandeshbhandare26 <bhandaresandesh26@gmail.com>
2025-07-16 17:37:33 +0530

    patterns

---
+++ d1/Pattern.java
@@ -0,0 +1,14 @@
+public class Pattern {
+    public static void main(String[] args){
+        int m=5;
+        int n=5;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*  ");
+            }
+        System.out.println("");
+        }
+    }
+
+    
+}
