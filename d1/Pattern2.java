21bf952bbf505f2091abaa7a9b7ee9cc093dc637
Sandeshbhandare26 <bhandaresandesh26@gmail.com>
2025-07-17 15:43:59 +0530

    journal patterns

--- d1/Pattern2.java
+++ d1/Pattern2.java
@@ -6,9 +6,9 @@ public class Pattern2 {
             for(int j=1;j<=m;j++){
 
                 if(i==1 || j==1 || i==4 || j==5)
-                {System.out.print("*");}
+                {System.out.print("* ");}
                 else{
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
                 
             }
