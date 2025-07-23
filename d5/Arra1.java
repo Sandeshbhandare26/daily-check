57ec1b3e4b9d8e468781e8d67cbe519819259cef
Sandeshbhandare26 <bhandaresandesh26@gmail.com>
2025-07-23 21:53:02 +0530

    array begins

---
+++ d5/Arra1.java
@@ -0,0 +1,10 @@
+public class Arra1 {
+    public static void main(String[] arg){
+        int[] arr=new int[2];
+        arr[0]=5;
+        arr[1]=10;
+        System.out.println("first element of array:"+arr[0]);
+        System.out.println("second element of array:"+arr[1]);
+    }
+    
+}
