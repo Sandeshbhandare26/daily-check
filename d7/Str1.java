a9a189d33fc7f9cf6add83e2cb47f3521fb4ba1e
Sandeshbhandare26 <bhandaresandesh26@gmail.com>
2025-07-27 22:58:12 +0530

    more inbuilt functions

--- d7/Str1.java
+++ d7/Str1.java
@@ -1,8 +1,11 @@
 public class Str1 {
     public static void main(String[] arg){
         StringBuilder sb=new StringBuilder("SandeshBhandare");
-        System.out.println(sb.charAt(2));
-        sb.setCharAt(4, 'i');
+        System.out.println(sb.charAt(2));//to print char at index 2
+        sb.setCharAt(4, 'i');//to replace a char
+        System.out.println(sb);//print string
+
+        sb.insert(0, 'I'); //to insert a char at some index
         System.out.println(sb);
         
 
