f2f466269b9c89e899446e89a7de597e5ebafbd2
Sandeshbhandare26 <bhandaresandesh26@gmail.com>
2025-07-27 23:09:21 +0530

    reverse a string using loop

--- d7/Str2.java
+++ d7/Str2.java
@@ -1,5 +1,18 @@
 public class Str2 {
     public static void main(String[] args) {
-        
+        StringBuilder sb=new StringBuilder("Loveyou3000");
+
+        for(int i=0;i<sb.length()/2;i++){
+            int front=i;
+            int back=sb.length()-i-1;
+
+            char frontch=sb.charAt(front);
+            char backch=sb.charAt(back);
+
+            sb.setCharAt(front, backch);
+            sb.setCharAt(back, frontch);
+        }
+    System.out.println(sb);
+    
     }
-}
+    }
