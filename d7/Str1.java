316dc93a6cdf9ff5b008488449a4b64926845eb6
Sandeshbhandare26 <bhandaresandesh26@gmail.com>
2025-07-27 22:54:34 +0530

    string builder

---
+++ d7/Str1.java
@@ -0,0 +1,10 @@
+public class Str1 {
+    public static void main(String[] arg){
+        StringBuilder sb=new StringBuilder("SandeshBhandare");
+        System.out.println(sb.charAt(2));
+        sb.setCharAt(4, 'i');
+        System.out.println(sb);
+        
+
+    }
+}
---
+++ d7/Str2.java
@@ -0,0 +1,5 @@
+public class Str2 {
+    public static void main(String[] args) {
+        
+    }
+}
